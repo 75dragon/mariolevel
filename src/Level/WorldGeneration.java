8eb8f46bd6b8b704f1c47d3a70d63bf5bfce53d1
Austin Cheng <austinbicheng@gmail.com>
2017-05-25 16:32:06 -0700

    pits random works?

--- src/Level/WorldGeneration.java
+++ src/Level/WorldGeneration.java
@@ -1,12 +1,16 @@
 package Level;
 
+import java.util.Random;
+
 public class WorldGeneration
 {
 	Tile[][] theWorld;
 	int xDim;
 	int yDim;
+	Random rand;
 	public WorldGeneration(int x, int y)
 	{
+		rand = new Random();
 		xDim = x;
 		yDim = y;
 		theWorld = new Tile[x][y];
@@ -14,25 +18,65 @@ public class WorldGeneration
 	
 	public void basics()
 	{
-		for (int i = 0; i < xDim; i++)
+		for (int i = 0; i < yDim; i++)
 		{
-			for (int j = 0; j < yDim - 1; j++)
+			for (int j = 0; j < xDim - 1; j++)
 			{
 				theWorld[i][j] = new Tile(i, j, 30, false);
 			}
 		}
 		for (int j = 0; j < xDim; j++)
 		{
-			theWorld[j][j] = new Tile(i, j, 30, false);
+			theWorld[yDim - 1][j] = new Tile(yDim - 1, j, 30, true);
 		}
 	}
 	
-	public void addPits()
+	/**
+	 * note: the first and last 15 tiles must not be pits:
+	 * 	its the end / beginning of the level!
+	 * @param x how many pits there should be
+	 * @param width how wide the pits are to jump over
+	 */
+	public void addPits( int i , int width )
 	{
+	
+		int[] holdPits = new int[i];
+		boolean check;
+		int hold;
+		int done = 0;
+		while( done < i)
+		{
+			check = true;
+			hold = rand.nextInt(yDim - 15) + 10;
+			for (int j = 0; j < done; j++)
+			{
+				if (Math.abs(hold - holdPits[j]) > width)
+				{
+					check = false;
+				}
+			}
+			if (check)
+			{
+				holdPits[done] = hold;
+				done++;
+			}
+		}
 		
+		for(int j = 0; j < i; j++ )
+		{
+			theWorld[yDim - 1][holdPits[j]] = new Tile(yDim - 1, j, 30, false);
+			theWorld[yDim - 1][holdPits[j + 1]] = new Tile(yDim - 1, j + 1, 30, false);
+			theWorld[yDim - 1][holdPits[j + 2]] = new Tile(yDim - 1, j + 2, 30, false);
+			theWorld[yDim - 1][holdPits[j + 3]] = new Tile(yDim - 1, j + 3, 30, false);
+			theWorld[yDim - 1][holdPits[j + 4]] = new Tile(yDim - 1, j + 4, 30, false);
+		}
 	}
 	
-	public void addRamdomBlocks()
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public void addRamdomBlocks(int amount)
 	{
 		
 	}
